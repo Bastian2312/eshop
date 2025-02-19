941095071f25430112a5dcc89066039399b40faa
Bastian2312 <bastian.adiputras@gmail.com>
2025-02-20 01:02:37 +0700

    fix: utility class has a non-private constructor

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EshopApplication {
+	private EshopApplication() {
+		// Prevent instantiation
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EshopApplication.class, args);
 	}
-
 }
