372624c1a2111de4fdbfb65fa273f9ea6ed3a82f
Bastian2312 <bastian.adiputras@gmail.com>
2025-02-20 23:39:40 +0700

    feat: created a base functional test class

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/BaseFunctionalTest.java
@@ -0,0 +1,31 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+public abstract class BaseFunctionalTest {
+
+    @LocalServerPort
+    protected int serverPort;
+
+    @Value("${app.baseUrl:http://localhost}")
+    protected String testBaseUrl;
+
+    protected String baseUrl;
+
+    @BeforeEach
+    void setUp() {
+        baseUrl = String.format("%s:%d", testBaseUrl, serverPort);
+    }
+}
+
+// USE "./gradlew functionalTest" IN TERMINAL TO RUN FUNCTIONAL TEST
--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -1,57 +1,29 @@
 package id.ac.ui.cs.advprog.eshop.functional;
 
-import io.github.bonigarcia.seljup.SeleniumJupiter;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-@ExtendWith(SeleniumJupiter.class)
-public class CreateProductFunctionalTest {
-
-    @LocalServerPort
-    private int serverPort;
-
-    @Value("${app.baseUrl:http://localhost}")
-    private String testBaseUrl;
-
-    private String baseUrl;
-
-    @BeforeEach
-    void setUp() {
-        baseUrl = String.format("%s:%d", testBaseUrl, serverPort);
-    }
+public class CreateProductFunctionalTest extends BaseFunctionalTest {
 
     @Test
     public void testCreateProduct(ChromeDriver driver) {
-        // Navigate to the create product page
         driver.get(baseUrl + "/product/create");
 
-        // Find and fill the product name input
         WebElement nameInput = driver.findElement(By.id("nameInput"));
         nameInput.sendKeys("Test Product");
 
-        // Find and fill the product quantity input
         WebElement quantityInput = driver.findElement(By.id("quantityInput"));
         quantityInput.sendKeys("10");
 
-        // Submit the form
         WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
         submitButton.click();
 
-        // Navigate to the product list page
         driver.get(baseUrl + "/product/list");
 
-        // Verify that the new product appears in the list
         WebElement table = driver.findElement(By.tagName("table"));
         String pageSource = table.getText();
 
