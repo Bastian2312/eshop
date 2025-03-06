b217fffabdf6b4b90427b02b1aaaa028d243bad5
Bastian2312 <bastian.adiputras@gmail.com>
2025-03-07 00:56:40 +0700

    fix: missing feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,11 +1,12 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Payment;
-import java.util.Collections;
+import org.springframework.stereotype.Repository;
 import java.util.Map;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Repository
 public class PaymentRepository {
     private final Map<String, Payment> paymentMap;
 
