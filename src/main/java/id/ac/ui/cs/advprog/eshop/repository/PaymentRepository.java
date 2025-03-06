861fdb7d71c43316a1698a7b6ffa30c0beae4047
Bastian2312 <bastian.adiputras@gmail.com>
2025-03-06 23:43:35 +0700

    package id.ac.ui.cs.advprog.eshop.repository;

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,28 +1,31 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Payment;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Collections;
 import java.util.Map;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class PaymentRepository {
-    private Map<String, Payment> paymentData;
+    private final Map<String, Payment> paymentMap;
 
     public PaymentRepository() {
-        paymentData = new HashMap<>();
+        paymentMap = new ConcurrentHashMap<>();
     }
 
     public Payment save(Payment payment) {
-        paymentData.put(payment.getId(), payment);
+        if(payment == null) {
+            throw new IllegalArgumentException("Payment cannot be null");
+        }
+        paymentMap.put(payment.getId(), payment);
         return payment;
     }
 
     public Payment findById(String id) {
-        return paymentData.get(id);
+        return paymentMap.get(id);
     }
 
     public List<Payment> findAll() {
-        return new ArrayList<>(paymentData.values());
+        return List.copyOf(paymentMap.values());
     }
 }
